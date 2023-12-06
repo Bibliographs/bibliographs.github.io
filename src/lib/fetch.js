34506e1a88edb547223338176cf553c83ea0dbe8
nbonfils <nils.bonfils@gmail.com>
2023-12-06 20:22:39 +0100

    fetch: Add function to fetch labels for the top refs

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -40,3 +40,37 @@ export const fetchWorks = async (query, fromYear, toYear, maxWorks) => {
   
   return {count, works};
 };
+
+export const fetchRefsLabels = async (openalexIds) => {
+  if (openalexIds.length === 0) return [];
+
+  const refsPerPage = 50;
+  const maxRefs = 500; // We'll only fetch the labels for the first maxRefs refs
+  const ids = [...openalexIds.slice(0, maxRefs)]; // Copy the array to prevent its destruction
+  const numReq = Math.ceil(ids.length / refsPerPage);
+
+  let refs = await Promise.all([...Array(numReq).keys()].map(async (i) => {
+    let data = {};
+    const idsStr = ids.splice(0, refsPerPage).join("|");
+
+    try {
+      const response = await fetch(
+        "https://api.openalex.org/works?" + new URLSearchParams({
+	  filter: `openalex:${idsStr}`,
+	  select: "id,display_name",
+          mailto: `****@****.com`,
+          "per-page": perPage,
+	  page: 1,
+	}));
+      if (!response.ok) {
+	throw new Error("Network response was not OK");
+      }
+      data = await response.json();
+    } catch (e) {
+      console.error(`Error while fetching refs:\n\t${e}`);
+    }
+    return data.results;
+  }));
+
+  return refs.flat().filter(ref => ref);
+};
