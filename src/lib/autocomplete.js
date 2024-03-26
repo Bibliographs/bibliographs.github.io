e2d5a9a6538b9ec40bdd28ebdffb60b3edb430c9
nbonfils <nils.bonfils@gmail.com>
2024-03-26 22:18:13 +0100

    Add topic

--- src/lib/autocomplete.js
+++ src/lib/autocomplete.js
@@ -36,3 +36,44 @@ export const autoCompleteConceptConfig = {
     }
   },
 };
+
+export const autoCompleteTopicConfig = {
+  placeHolder: 'Search for Topics...',
+  data: {
+    src: async (query) => {
+      try {
+	const response = await fetch(
+          'https://api.openalex.org/autocomplete/topics?' + new URLSearchParams({
+	    q: query,
+            mailto: `****@****.com`,
+	  }));
+	if (!response.ok) {
+	  throw new Error('Network response was not OK');
+	}
+	const data = await response.json();
+	return data.results;
+      } catch (e) {
+	console.error(`Error while fetching topics to autocomplete:\n\t${e}`);
+	return e;
+      }
+    },
+    keys: ['display_name'],
+    cache: false,
+  },
+  debounce: 300,
+  resultItem: {
+    tabSelect: true,
+    noResults: true,
+    highlight: true,
+  },
+  events: {
+    input: {
+      navigate: (event) => {
+	const selection = event.detail.selection.value;
+	event.target.value = selection.display_name;
+      }
+    }
+  },
+};
+
+
